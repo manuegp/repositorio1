2e5bbfb82ddcc9a7ed589a01e2606645d36e2e56
manue <manue@DESKTOP-PRI5I9E>
2020-03-24 11:07:42 +0100

    Subir archivo a Github

--- proyecto1/src/proyecto1/Proyecto1.java
+++ proyecto1/src/proyecto1/Proyecto1.java
@@ -15,6 +15,8 @@ public class Proyecto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("fhsda");    }
+        System.out.println("fhsda");    
+        System.out.println(2+2);
+    }
     
 }
