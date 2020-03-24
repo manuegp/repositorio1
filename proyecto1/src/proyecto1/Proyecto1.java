72a188936ebf0f47e86ee9a8e42fbcc7c206c49f
manuegp <62593980+manuegp@users.noreply.github.com>
2020-03-24 11:12:54 +0100

    Update Proyecto1.java

--- proyecto1/src/proyecto1/Proyecto1.java
+++ proyecto1/src/proyecto1/Proyecto1.java
@@ -16,7 +16,7 @@ public class Proyecto1 {
      */
     public static void main(String[] args) {
         System.out.println("fhsda");    
-        System.out.println(2+2);
+        System.out.println(2+1);
     }
     
 }
